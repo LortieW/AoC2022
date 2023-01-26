aab37e4f2aa06978cc98919dc3ae97e5ee5033f1
William Lortie <lortie.w@gmail.com>
2023-01-26 11:38:00 -0500

    Complete Day 3 (b)

---
+++ day_3b/app.py
@@ -0,0 +1,19 @@
+from itertools import islice
+def main():
+    total_priority = 0
+    with open("./input.txt", "r") as f:
+        for head_line in f:
+            elf_A, elf_B, elf_C = set(head_line[:-1]), set(next(f)[:-1]), set(next(f)[:-1])
+            print(f'{elf_A},{elf_B},{elf_C}')
+            for item in elf_A:
+                if item in elf_B and item in elf_C:
+                    total_priority += get_priority(item)
+                    break
+    print(total_priority)
+               
+def get_priority(item: str) -> int:
+    ascii_value = ord(item)
+    return ascii_value - 96 if item.islower() else ascii_value - 38
+
+if __name__ == '__main__':
+    main()

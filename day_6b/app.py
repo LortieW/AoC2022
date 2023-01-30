dc7d860e472c2fc312e54de0cfb1b2aeafb73da3
William Lortie <lortie.w@gmail.com>
2023-01-30 12:37:45 -0500

    Complete Day 6 (b)

---
+++ day_6b/app.py
@@ -0,0 +1,17 @@
+def main():
+    with open("./input.txt", "r") as f:
+        marker = ""
+        index = 0
+        for line in f:
+            for character in line:
+                i = marker.find(character)
+                if i >= 0:
+                    marker = marker[i+1:]
+                marker += character
+                index += 1
+                if len(marker) == 14:
+                    break
+        print(index)
+
+if __name__ == '__main__':
+    main()
